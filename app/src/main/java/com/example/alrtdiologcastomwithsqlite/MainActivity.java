0447f7d0e9daca2fc1fc20aaa253b9f1a50f0cd3
khalid063 <khalid.ahmad063@gmail.com>
2021-12-26 15:09:55 +0600

    Castom dialog done

--- app/src/main/java/com/example/alrtdiologcastomwithsqlite/MainActivity.java
+++ app/src/main/java/com/example/alrtdiologcastomwithsqlite/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.alrtdiologcastomwithsqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewUserName, textViewPassword;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewUserName = findViewById(R.id.tv_username);
+        textViewPassword = findViewById(R.id.tv_password);
+        button = findViewById(R.id.but_open_diolog);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDiolog();
+            }
+        });
+
     }
+
+    public void openDiolog(){
+        ExampleDiolog exampleDiolog = new ExampleDiolog();
+        exampleDiolog.show(getSupportFragmentManager(), "example diolog");
+
+    }
+
 }
